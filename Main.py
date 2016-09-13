b3eea8ceb6dae711f4b84c151e92106b7307dc46
krishna <krishna1madhur@gmail.com>
2016-09-13 03:07:22 -0500

    F2 Generation

--- Main.py
+++ Main.py
@@ -13,8 +13,51 @@ listOfTrasactions = []
 listOfItems = []
 listL = []
 listF1 = []
-listC2 = []
+listCk = []
+listFk = []
+dictionaryFk = {}
 
+def msCandidateGen():
+    print("In C3")
+
+def msApriori():
+    # CALL TO THE INIT_PASS FUNCTION
+    init_pass()
+    # CALL TO COMPUTE THE F1 FREQUENT ITEM SETS
+    computeF1()
+    k = 2
+#    while(len(dictionaryFk[k-1]) != 0):
+    while( k == 2):
+        dictionaryCount = OrderedDict({})
+        if k == 2:
+            computeC2()
+            print("C2: ", listCk)
+        else:
+            msCandidateGen()
+        for transaction in listOfTrasactions:
+            for candidate in listCk:
+                lengthOfCandidate = len(candidate)
+                for eachCandidate in candidate:
+                    if eachCandidate in transaction:
+                        lengthOfCandidate = lengthOfCandidate - 1
+                candidateString = ','.join(candidate)
+                if(lengthOfCandidate == 0):
+                    if(candidateString in dictionaryCount.keys()):
+                        dictionaryCount[candidateString] = dictionaryCount[candidateString] + 1
+                    else:
+                        dictionaryCount[candidateString] = 1
+                elif(candidateString not in dictionaryCount.keys()):
+                    dictionaryCount[candidateString] = 0
+
+        print("\nDictionary Count: ",dictionaryCount)
+        listOfC = []
+        for c in listCk:
+            stringC = ','.join(c)
+            if(dictionaryCount[stringC]/len(listOfTrasactions) >= dictionaryMIS[ c[0] ]):
+                listOfC.append(c)
+        dictionaryFk[k] = listOfC
+        k = k + 1
+        print("F2: ", dictionaryFk)
 
 def init_pass():
     # SCAN THE DATA AND RECORD THE SUPPORT
@@ -40,16 +83,10 @@ def init_pass():
     for key,value in dictionaryMIS.items():
         if( dictionaryMIS[listL[0]] <= dictionaryMIS[key] and (dictionaryI[key] >= dictionaryMIS[listL[0]]) and key != listL[0] ):
             listL.append(key)
-    # STEPS FOR FORMING F1
-    for item in listL:
-        if(dictionaryI[item] >= dictionaryMIS[item]):
-            listF1.append(item)
-    print("Frequent 1-itemsets \n")
-    for item in listF1:
-        print(listOfItems.count(item) ,': {', item ,'}\n')
 
 # FUNCTION TO COMPUTE THE LEVEL 2 CANDIDATE GENERATION
 def computeC2():
+    del listCk[:]
     for itemL in range(0,len(listL)):
         if(dictionaryI[ listL[itemL] ] >= dictionaryMIS[ listL[itemL]] ):
             for itemH in range(itemL+1, len(listL)):
@@ -57,8 +94,16 @@ def computeC2():
                     twoItemsList = []
                     twoItemsList.append(listL[itemL])
                     twoItemsList.append(listL[itemH])
-                    listC2.append(twoItemsList)
-    print(listC2)
+                    listCk.append(twoItemsList)
+
+def computeF1():
+    # STEPS FOR FORMING F1
+    for item in listL:
+        if (dictionaryI[item] >= dictionaryMIS[item]):
+            listF1.append(item)
+    print("Frequent 1-itemsets \n")
+    for item in listF1:
+        print(listOfItems.count(item), ': {', item, '}\n')
 
 if __name__ == "__main__":
     # IMPORT TRANSACTIONS AND OTHER PARAMETERS FROM INPUT TEXT FILES
@@ -81,10 +126,7 @@ if __name__ == "__main__":
             transaction = transaction.partition('{')[-1].rpartition('}')[0]
             transactionList = [x.strip() for x in transaction.split(',')]
             listOfTrasactions.append(transactionList)
-
     # SORT THE MIS VALUES AND STORE IN A DICTIONARY
     dictionaryMIS = OrderedDict(sorted(dictionaryMIS.items(), key=lambda t: t[1]))
 
-    # CALL TO THE INIT_PASS FUNCTION
-    init_pass()
-    computeC2()
+    msApriori()
